61cb256b7cf1957d080505ba0abf56e10bd34680
Swilsondi <165120193+Swilsondi@users.noreply.github.com>
2024-07-04 14:19:41 -0400

    finished

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const body = document.getElementsByTagName("body")[0]
+const greenButton = document.getElementById("green");
+const redButton = document.getElementById("red");
+const blueButton = document.getElementById("blue");
 
 function setColor(name) {
     body.style.backgroundColor = name;
 }
 
 function randomColor() {
-    const red = Math.random() * 255;
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color;
 }
 
+greenButton.addEventListener("click", () => setColor("green"));
+redButton.addEventListener("click", () => setColor("red"));
+blueButton.addEventListener("click", () => setColor("blue"));
+randomColor();
