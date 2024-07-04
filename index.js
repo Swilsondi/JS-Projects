8767b25189643ccd11231972e9544bea6ab5d3bd
Swilsondi <165120193+Swilsondi@users.noreply.github.com>
2024-07-04 13:41:28 -0400

    added json file

---
+++ index.js
@@ -0,0 +1,10 @@
+const body = document.getElementsByTagName("body")[0]
+
+function setColor(name) {
+    body.style.backgroundColor = name;
+}
+
+function randomColor() {
+    const red = Math.random() * 255;
+}
+
